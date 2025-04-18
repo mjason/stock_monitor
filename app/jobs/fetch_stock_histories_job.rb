938ab7266235fd96045b350dea2f629e6a1636f9
MJ <tywf91@gmail.com>
2025-04-18 11:40:10 +0800

    fix somebug

--- app/jobs/fetch_stock_histories_job.rb
+++ app/jobs/fetch_stock_histories_job.rb
@@ -2,6 +2,6 @@ class FetchStockHistoriesJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    StockDailyPrice.sync_all StockStrategy.where(active: true).map(&:stock_symbol)
+    StockDailyPrice.sync StockStrategy.where(active: true).map(&:stock_symbol)
   end
 end
